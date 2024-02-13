e726f37510cebbf808849b9b5c039dba51d9b0f3
Masum Mollo <freelancermasummolla@gmail.com>
2024-02-13 18:47:17 +0600

    assets and folders

---
+++ resources/views/layout/sidenav-layout.blade.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <title></title>
+
+    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
+    <link href="{{asset('backend/css/bootstrap.css')}}" rel="stylesheet" />
+    <link href="{{asset('backend/css/animate.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('backend/css/fontawesome.css')}}" rel="stylesheet" />
+    <link href="{{asset('backend/css/style.css')}}" rel="stylesheet" />
+    <link href="{{asset('backend/css/toastify.min.css')}}" rel="stylesheet" />
+
+    <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css')}}" rel="stylesheet" />
+
+    <link href="{{asset('backend/css/jquery.dataTables.min.css')}}" rel="stylesheet" />
+    <script src="{{asset('backend/js/jquery-3.7.0.min.js')}}"></script>
+    <script src="{{asset('backend/js/jquery.dataTables.min.js')}}"></script>
+
+    <script src="{{asset('backend/js/toastify-js.js')}}"></script>
+    <script src="{{asset('backend/js/axios.min.js')}}"></script>
+    <script src="{{asset('backend/js/config.js')}}"></script>
+    <script src="{{asset('backend/js/bootstrap.bundle.js')}}"></script>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+<div id="loader" class="LoadingOverlay d-none">
+    <div class="Line-Progress">
+        <div class="indeterminate"></div>
+    </div>
+</div>
+
+<nav class="navbar fixed-top px-0 shadow-sm bg-white">
+    <div class="container-fluid">
+
+        <span class="navbar-brand flex items-center">
+            <span class="icon-nav m-0 h5" onclick="MenuBarClickHandler()">
+                <img class="nav-logo-sm mx-2"  src="{{asset('backend/images/menu.svg')}}" alt="logo"/>
+            </span>
+            <a href="{{route('home')}}">
+                <img class="nav-logo mx-2" src="{{asset('backend/images/logo.png')}}" alt="logo"/>
+            </a>
+        </span>
+
+        <div class="float-right h-auto d-flex">
+            <button class="btn btn-primary px-3 py-2" style="margin-right: 5px;">Admin</button>
+            <div class="user-dropdown">
+                <img class="icon-nav-img" src="{{asset('backend/images/user.webp')}}" alt=""/>
+                <div class="user-dropdown-content ">
+                    <div class="mt-4 text-center flex flex-column items-center">
+                        <img class="icon-nav-img" src="{{asset('backend/images/user.webp')}}" alt=""/>
+                        <h6>User Name</h6>
+                        <hr class="user-dropdown-divider  p-0"/>
+                    </div>
+                    <a href="{{ route('userProfile') }}" class="side-bar-item">
+                        <span class="side-bar-item-caption">Profile</span>
+                    </a>
+                    <a onclick="logout()" href="" class="side-bar-item">
+                        <span class="side-bar-item-caption">Logout</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+<div id="sideNavRef" class="side-nav-open">
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-graph-up"></i>
+        <span class="side-bar-item-caption">Dashboard</span>
+    </a>
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-people"></i>
+        <span class="side-bar-item-caption">Companies</span>
+    </a>
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-list-nested"></i>
+        <span class="side-bar-item-caption">Jobs</span>
+    </a>
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-bag"></i>
+        <span class="side-bar-item-caption">Employee</span>
+    </a>
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-currency-dollar"></i>
+        <span class="side-bar-item-caption">Blog</span>
+    </a>
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-receipt"></i>
+        <span class="side-bar-item-caption">Pages</span>
+    </a>
+
+    <a href="" class="side-bar-item">
+        <i class="bi bi-file-earmark-bar-graph"></i>
+        <span class="side-bar-item-caption">Plugin</span>
+    </a>
+
+
+</div>
+
+
+<div id="contentRef" class="content">
+    @yield('content')
+</div>
+
+
+
+<script>
+    function MenuBarClickHandler() {
+        let sideNav = document.getElementById('sideNavRef');
+        let content = document.getElementById('contentRef');
+        if (sideNav.classList.contains("side-nav-open")) {
+            sideNav.classList.add("side-nav-close");
+            sideNav.classList.remove("side-nav-open");
+            content.classList.add("content-expand");
+            content.classList.remove("content");
+        } else {
+            sideNav.classList.remove("side-nav-close");
+            sideNav.classList.add("side-nav-open");
+            content.classList.remove("content-expand");
+            content.classList.add("content");
+        }
+    }
+
+    async function logout(){
+        try{
+            let res = await axios.get('/logout',HeaderToken());
+            successToast(res.data.message);
+
+            localStorage.clear();
+            sessionStorage.clear();
+            window.location.href = '/user-login';
+        }
+        catch (e){
+            errorToast(e.response.data.message);
+        }
+    }
+
+</script>
+
+</body>
+</html>
---
+++ resources/views/pages/frontend/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+
+ <h1 class="text-3xl font-bold underline">
+     Hello world!
+ </h1>
+
+@endsection
