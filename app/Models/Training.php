26e52139621b79670ccd03acd740b2cfa1b34b3f
Masum Mollo <freelancermasummolla@gmail.com>
2024-02-29 23:53:32 +0600

    Candidate Training - Table, Model

---
+++ app/Models/Training.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Training extends Model
+{
+    protected $table = 'trainings';
+    protected $fillable = [
+        'title',
+        'institute',
+        'completion_year',
+        'user_id'
+    ];
+}
