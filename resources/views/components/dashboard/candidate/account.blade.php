8dd3be143f246f611cd9b9045d43287516fd8ad4
Masum Mollo <freelancermasummolla@gmail.com>
2024-03-09 22:17:47 +0600

    Profile Img script

--- resources/views/components/dashboard/candidate/account.blade.php
+++ resources/views/components/dashboard/candidate/account.blade.php
@@ -10,47 +10,54 @@
         </div>
 
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6 border-b py-4">
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input type="email" id="email" readonly placeholder="Jon Deo" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+        <div>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6 border-b py-4">
+                <div>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="email" id="email" readonly placeholder="Jon Deo" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+                </div>
+                <div>
+                    <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                    <input type="text" id="firstName" placeholder="David Deo" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+                </div>
+                <div>
+                    <label for="lastName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                    <input type="text" id="lastName" placeholder="Joly Deo" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+                </div>
+                <div>
+                    <label for="mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number</label>
+                    <input type="number" id="mobile" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+                </div>
             </div>
-            <div>
-                <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                <input type="text" id="firstName" placeholder="David Deo" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6 mb-5 border-b py-4">
+                <div>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Password</label>
+                    <input type="password" id="email" readonly class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+                </div>
+                <div>
+                    <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                    <input type="password" id="firstName" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+                </div>
             </div>
-            <div>
-                <label for="lastName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                <input type="text" id="lastName" placeholder="Joly Deo" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
-            </div>
-            <div>
-                <label for="mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number</label>
-                <input type="number" id="mobile" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
+
+            <div class="flex flex-col gap-4 py-4  lg:flex-row">
+                <div class="shrink-0 w-32  sm:py-4">
+                    <p class="mb-auto font-medium">Avatar</p>
+                    <p class="text-sm text-gray-600">Change your avatar</p>
+                </div>
+                <div class="flex h-56 w-full flex-col items-center justify-center gap-4 rounded-xl border border-dashed border-gray-300 p-5 text-center">
+                    <img id="oldImg" src="{{asset('backend/images/default.jpg')}}" class="h-16 w-16 rounded-full" alt="Img">
+                    <p class="text-sm text-gray-600">Drop your desired image file here to start the upload</p>
+                    {{--                <input type="file" class="max-w-full rounded-lg px-2 pb-5 font-medium text-blue-600 outline-none ring-blue-600 focus:ring-1">--}}
+                    <input oninput="oldImg.src=window.URL.createObjectURL(this.files[0])"  type="file" class="pb-4 form-control" id="productImgUpdate">
+
+                    <input type="text" class="d-none" id="updateID">
+                    <input type="text" class="d-none" id="filePath">
+                </div>
             </div>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6 mb-5 border-b py-4">
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Password</label>
-                <input type="password" id="email" readonly class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
-            </div>
-            <div>
-                <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-                <input type="password" id="firstName" class="mb-2 w-full rounded-md border bg-white px-2 py-2 outline-none ring-blue-600 sm:mr-4 sm:mb-0 focus:ring-1" />
-            </div>
-        </div>
-
-        <div class="flex flex-col gap-4 py-4  lg:flex-row">
-            <div class="shrink-0 w-32  sm:py-4">
-                <p class="mb-auto font-medium">Avatar</p>
-                <p class="text-sm text-gray-600">Change your avatar</p>
-            </div>
-            <div class="flex h-56 w-full flex-col items-center justify-center gap-4 rounded-xl border border-dashed border-gray-300 p-5 text-center">
-                <img src="https://componentland.com/images/ddHJYlQqOzyOKm4CSCY8o.png" class="h-16 w-16 rounded-full" alt="Img">
-                <p class="text-sm text-gray-600">Drop your desired image file here to start the upload</p>
-                <input type="file" class="max-w-full rounded-lg px-2 pb-5 font-medium text-blue-600 outline-none ring-blue-600 focus:ring-1">
-            </div>
-        </div>
         <div class="flex justify-end py-4 sm:hidden">
             <button class="mr-2 rounded-lg border-2 px-4 py-2 font-medium text-gray-500 focus:outline-none focus:ring hover:bg-gray-200">Cancel</button>
             <button class="rounded-lg border-2 border-transparent bg-blue-600 px-4 py-2 font-medium text-white focus:outline-none focus:ring hover:bg-blue-700">Save</button>
@@ -64,7 +71,9 @@
 
     getProfileAccount();
     async function getProfileAccount(){
+
         try{
+
             showLoader();
             let res=await axios.get("/user-profile",HeaderToken());
             hideLoader();
@@ -74,6 +83,10 @@
                 document.getElementById('firstName').value=res.data['firstName'];
                 document.getElementById('lastName').value=res.data['lastName'];
                 document.getElementById('mobile').value=res.data['mobile'];
+
+                document.getElementById('updateID').value=res.data['id'];
+                document.getElementById('filePath').value=res.data['file_path'];
+                // document.getElementById('oldImg').src=res.data['file_path'];
             }
             else{
                 errorToast(res.data['message']);
@@ -87,14 +100,37 @@
 
 
     async function onUpdateAccount(){
-        let PostBody={
-            "firstName":document.getElementById('firstName').value,
-            "lastName":document.getElementById('lastName').value,
-            "mobile":document.getElementById('mobile').value,
+
+        let updateID=document.getElementById('updateID').value;
+        let filePath=document.getElementById('filePath').value;
+        let productImgUpdate = document.getElementById('productImgUpdate').files[0];
+        let firstName=document.getElementById('firstName').value;
+        let lastName=document.getElementById('lastName').value;
+        let mobile=document.getElementById('mobile').value;
+
+        // let PostBody={
+        //     "firstName":document.getElementById('firstName').value,
+        //     "lastName":document.getElementById('lastName').value,
+        //     "mobile":document.getElementById('mobile').value,
+        //     "img":document.getElementById('filePath').value,
+        // }
+
+        let formData=new FormData();
+        formData.append('img',productImgUpdate)
+        formData.append('id',updateID)
+        formData.append('firstName',firstName)
+        formData.append('lastName',lastName)
+        formData.append('mobile',mobile)
+        formData.append('img',filePath)
+
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
         }
 
         showLoader();
-        let res=await axios.post("/user-update",PostBody,HeaderToken());
+        let res=await axios.post("/user-update",formData,config,HeaderToken());
         hideLoader();
 
         if(res.data['status']==="success"){
@@ -105,6 +141,7 @@
             errorToast(res.data['message'])
         }
     }
+
 
     async function onCancelAccount(){
         if(confirm("Are you sure to cancel?")){
